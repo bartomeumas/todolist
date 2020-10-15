cd472231a542f5785988ebeb595f6799c5e29a3f
Bartomeu Mas <bartomeumas00@gmail.com>
2020-10-14 23:11:04 -0400

    Initial Commit

---
+++ ToDoList/UrlHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDoList
+{
+    public static class UrlHelperExtensions
+    {
+        public static string ContentVersioned(this UrlHelper self, string contentPath)
+        {
+            string versionedContentPath = contentPath + "?v=" + Assembly.GetAssembly(typeof(UrlHelperExtensions)).GetName().Version.ToString();
+            return self.Content(versionedContentPath);
+        }
+    }
+
+}
